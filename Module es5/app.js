5d6c5f592de2e1a924a574dada05bb38ac1f7b63
Robbert Wolfs <robbertwolfs@gmail.com>
2015-10-07 13:05:40 +0200

    update module es5

--- Module es5/app.js
+++ Module es5/app.js
@@ -2,41 +2,47 @@
     'use strict'
     $(document).ready(function() {
 
-        $('.mul').on('click', function(e) {
+        var $cForm = $('#calculatorForm');
+        var $mul = $cForm.find('.mul');
+        var $add = $cForm.find('.add');
+        var $result = $cForm.find('.result');
+
+        $mul.on('click', function(e) {
             e.preventDefault();
             var values = getValues();
             calculate('mul', values);
         });
 
-        $('.add').on('click', function(e) {
+        $add.on('click', function(e) {
             e.preventDefault();
             var values = getValues();
             calculate('add', values);
         });
-    });
 
-    function getValues() {
-        return [parseInt($('#number1').val()),
-            parseInt($('#number2').val())
-        ]
-    }
-
-    function calculate(cMethod, values) {
-        var result;
-        if (values[0] && values[1]) {
-            if (cMethod == 'add') {
-                result = calc.add(values[0], values[1]);
-            } else {
-                result = calc.mul(values[0], values[1]);
-            }
-            $('.result').text('Result : ' + result);
-        } else {
-            showError();
+        function getValues() {
+            return [parseInt($('#number1').val()),
+                parseInt($('#number2').val())
+            ]
         }
-    }
 
-    function showError() {
-        $('.result').text('Gelieve alle inputs correct in te vullen.');
-    }
+        function calculate(cMethod, values) {
+            var result;
+            if (values[0] && values[1]) {
+                if (cMethod == 'add') {
+                    result = calc.add(values[0], values[1]);
+                } else {
+                    result = calc.mul(values[0], values[1]);
+                }
+                $result.text('Result : ' + result);
+            } else {
+                showError();
+            }
+        }
+
+        function showError() {
+            $result.text('Gelieve alle inputs correct in te vullen.');
+        }
+
+    });
 
 })(jQuery, calc);
