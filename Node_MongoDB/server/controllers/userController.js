80c543f50e64d8cce198be5673358394d424c44b
Robbert Wolfs <robbertwolfs@gmail.com>
2015-10-14 15:07:59 +0200

    promises

--- Node_MongoDB/server/controllers/userController.js
+++ Node_MongoDB/server/controllers/userController.js
@@ -1,39 +1,102 @@
 var _ = require('underscore');
 var UserModel = require('../models/users');
 var userMapper = require('../mappers/users');
+var Q = require('q');
+
+
+var repository = {
+
+    findAll: function (query, pageSize, page, sort) {
+        var deferred = Q.defer();
+
+        UserModel.find(query).limit(pageSize).skip(pageSize * page).sort(sort).exec(function (err, users) {
+
+            if (err)
+                return deferred.reject(err);
+
+
+            return deferred.resolve(users);
+        });
+
+        return deferred.promise;
+
+    },
+
+    findOne: function (query) {
+        var deferred = Q.defer();
+        UserModel.findOne(query, function (err, user) {
+            if (err)
+                return deferred.resolve(err);
+            deferred.resolve(user);
+        });
+        return deferred.promise;
+    },
+    save: function (model) {
+        var deferred = Q.defer();
+        model.save(function (err) {
+            if (err)
+                return deferred.reject(err);
+            deferred.resolve(model);
+        });
+        return deferred.promise;
+    }
+};
 
 module.exports = {
 
     findAll: function (req, res, next) {
         var pageSize = req.query.pageSize || 100;
         var page = req.query.page || 0;
+        var sort = req.query.sort || '+name';
 
-        UserModel.find({}).limit(pageSize).skip(pageSize * page).sort(req.query.sort).exec(function (err, users) {
-            var filteredUsers = [];
-            filteredUsers = _.map(users, function (user) {
-                return userMapper.map(user);
+        repository.findAll({}, pageSize, page, sort)
+            .then(function (users) {
+
+
+                filteredUsers = _.map(users, function (user) {
+                    return userMapper.map(user);
+                });
+
+                return res.status(200).send(filteredUsers);
+            })
+            .catch(function () {
             });
-            return res.status(200).send(filteredUsers);
-        });
     },
 
     findOne: function (req, res, next) {
-        UserModel.findOne({
-            _id: req.params.id
-        }, function (err, user) {
+
+        repository.findOne(
+            {
+                _id: req.params.id
+            })
+            .then(function (user) {
+                if (!user) {
+                    return next(error(404));
+                }
+                return res.status(200).send(userMapper.map(user));
+
+            })
+            .catch(function (err) {
+                next(err);
+            });
 
 
-            //error afhandeling
-            //if (err) { return next(err); }
-            if (err || !user) {
-                return next(error(404));
-            }
-
-            return res.status(200).send(userMapper.map(user));
-        });
+        //UserModel.findOne({
+        //    _id: req.params.id
+        //}, function (err, user) {
+        //
+        //
+        //    //error afhandeling
+        //    //if (err) { return next(err); }
+        //    if (err || !user) {
+        //        return next(error(404));
+        //    }
+        //
+        //    return res.status(200).send(userMapper.map(user));
+        //});
     },
 
-    create : function (req, res, next) {
+    create: function (req, res, next) {
         var user = new UserModel({ // dit geeft al een nieuwe _id per product
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -52,34 +115,66 @@ module.exports = {
         });
     },
 
-    update : function (req, res, next) {
-
-        UserModel.findOne({
+    update: function (req, res, next) {
+        repository.findOne({
             _id: req.params.id
-        }, function (err, user) {
+        })
+            .then(function (user) {
 
-            //error afhandeling
-            if (err || !user) {
-                return next(error(404));
-            }
+                if (!user)
+                    return next(error(404));
 
-            user.id = req.params.id;
-            user.firstName = req.body.firstName;
-            user.lastName = req.body.lastName;
-            user.age = req.body.age;
-            user.email = req.body.email;
-            user.homeAddress.addressLine = req.body.addressLine;
-            user.homeAddress.city = req.body.city;
-            user.homeAddress.zip = req.body.zip;
+                user.id = req.params.id;
+                user.firstName = req.body.firstName;
+                user.lastName = req.body.lastName;
+                user.age = req.body.age;
+                user.email = req.body.email;
+                user.homeAddress.addressLine = req.body.addressLine;
+                user.homeAddress.city = req.body.city;
+                user.homeAddress.zip = req.body.zip;
 
-            user.save(function () {
-                return res.status(201).send(userMapper.map(user));
+                return repository.save(user);
+            })
+            .then(function (model) {
+                // called after repository.save() is finished
+
+                console.log('then');
+                return res.status(201).send(userMapper.map(model));
+            })
+            .catch(function (err) {
+                return next(err);
             });
 
-        });
+
+        //UserModel.findOne({
+        //    _id: req.params.id
+        //}, function (err, user) {
+        //
+        //    //error afhandeling
+        //    if (err || !user) {
+        //        return next(error(404));
+        //    }
+        //
+        //    user.id = req.params.id;
+        //    user.firstName = req.body.firstName;
+        //    user.lastName = req.body.lastName;
+        //    user.age = req.body.age;
+        //    user.email = req.body.email;
+        //    user.homeAddress.addressLine = req.body.addressLine;
+        //    user.homeAddress.city = req.body.city;
+        //    user.homeAddress.zip = req.body.zip;
+        //
+        //    user.save(function (err) {
+        //
+        //        if(err) return next(err);
+        //
+        //        return res.status(201).send(userMapper.map(user));
+        //    });
+
+        //});
     },
 
-    delete : function (req, res, next) {
+    delete: function (req, res, next) {
         UserModel.findOne({
             _id: req.params.id
         }, function (err, user) {
