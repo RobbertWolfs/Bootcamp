c28409a71ff50456d97d1ec9f57ef82ed1b429dd
Robbert Wolfs <robbertwolfs@gmail.com>
2015-10-26 11:05:37 +0100

    unit testing for angular

--- Angular/unit testing/personsApp-starter/app/scripts/filters/dateFilter.js
+++ Angular/unit testing/personsApp-starter/app/scripts/filters/dateFilter.js
@@ -5,15 +5,15 @@
         .module('app')
         .filter('dateFormat', function () {
             return function (input) {
-                if (!input)
+                if (!input) // undefined, null, string
                     return input;
 
-                if(!angular.isDate(input) && !angular.isString(input))
+                if(!angular.isDate(input) && !angular.isString(input)) // is not string or no date
                     return input;
 
                 var momentDate = moment(input);
 
-                if(!momentDate.isValid())
+                if(!momentDate.isValid()) // no valid date
                     return input;
 
                 return momentDate.format('DD-MM-YYYY');
